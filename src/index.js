5195ba25a139fb2b14e4ed5822de4e6d47530d9a
ani697 <aniket697@gmail.com>
2020-10-13 20:40:43 +0530

    post call set

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 //module.export = require('./lib/gather.js')
 
-const gather = require('../lib/gather.js');
+const GatherCall = require('../lib/gather.js');
+console.log('GatherCallSecond: ', GatherCall);
+
+let payload = {
+    name : "morpheus",
+    job : "leader"
+}
+const  gatherUser = new GatherCall('GET', 'https://reqres.in/api/users');
+const  gatherPostUser = new GatherCall('POST', 'https://reqres.in/api/users', payload);
+const  gatherPoke = new GatherCall('GET', 'https://pokeapi.co/api/v2/pokemon/ditto');
+
+gatherUser.getCall.then((res) => console.log('resUser ',res))
+gatherPostUser.getCall.then((res) => console.log('resPOSTUser ',res))
+gatherPoke.getCall.then((res) => console.log('resPoke ',res))
+
 console.log('Hello world')
 
-gather();
+//gather();
