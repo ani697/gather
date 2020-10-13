41cce81ce2292ecc5550906e86a034e57f619fbb
ani697 <aniket697@gmail.com>
2020-10-13 21:36:13 +0530

    header to get call is set

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 //module.export = require('./lib/gather.js')
 
 const GatherCall = require('../lib/gather.js');
-console.log('GatherCallSecond: ', GatherCall);
 
 let payload = {
     name : "morpheus",
     job : "leader"
 }
-const  gatherUser = new GatherCall('GET', 'https://reqres.in/api/users');
-const  gatherPostUser = new GatherCall('POST', 'https://reqres.in/api/users', payload);
-const  gatherPoke = new GatherCall('GET', 'https://pokeapi.co/api/v2/pokemon/ditto');
 
-gatherUser.getCall.then((res) => console.log('resUser ',res))
-gatherPostUser.getCall.then((res) => console.log('resPOSTUser ',res))
-gatherPoke.getCall.then((res) => console.log('resPoke ',res))
+  
+// const  gatherUser = new GatherCall('GET', 'https://reqres.in/api/users');
+// const  gatherPostUser = new GatherCall('POST', 'https://reqres.in/api/users', payload);
+// gatherUser.getCall.then((res) => console.log('resUser ',res))
+// gatherPostUser.postCall.then((res) => console.log('resPOSTUser ',res))
 
-console.log('Hello world')
+
+
 
 //gather();
